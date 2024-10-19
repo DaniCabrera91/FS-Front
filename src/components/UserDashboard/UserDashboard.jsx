c65bafe9196cf0422557e8b0f06b0bf2103acd36
aitziberE <eskizabel.aitziber@gmail.com>
2024-10-19 21:03:33 +0200

    integrate previous userDashboard view with new one

--- src/components/UserDashboard/UserDashboard.jsx
+++ src/components/UserDashboard/UserDashboard.jsx
@@ -1,14 +1,20 @@
 import React from 'react'
 import { useNavigate } from 'react-router-dom'
-import ProjectPlannerCard from '../ProjectPlannerCard/ProjectPlannerCard' // Asegúrate de que la ruta sea correcta
 import './UserDashboard.styled.scss'
-import App from './test/App'
+import AccountSummary from './AccountSummary'
+import FinanceOverview from './FinanceOverview'
+import TransactionList from './TransactionList'
+
+import { ChevronRight } from 'lucide-react'
+import ProjectPlannerCard from '../ProjectPlannerCard/ProjectPlannerCard'
 
 const Home = () => {
   const navigate = useNavigate()
 
-  const handleMyFinancesClick = () => {
-    navigate('/user/finances')
+  const handleClick = () => {
+    setTimeout(() => {
+      navigate('/user/finances')
+    }, 250)
   }
 
   // Ejemplos de proyectos ficticios de ahorro para mostrar en el dashboard
@@ -32,31 +38,42 @@ const Home = () => {
   ]
 
   return (
-    // <div>
-    //   <h1>Home Dashboard</h1>
-
-    //   <button onClick={handleMyFinancesClick} className='financesBtn'>
-    //     Mis Finanzas
-    //   </button>
-
-    //   {/* Sección de Proyectos */}
-    //   <h2>Mis Objetivos de Ahorro</h2>
-    //   <div className='project-cards'>
-    //     {projects.map((project) => (
-    //       <div key={project.id} style={{ marginBottom: '20px' }}>
-    //         <ProjectPlannerCard
-    //           projectName={project.name}
-    //           estimatedCost={project.estimatedCost}
-    //           monthlySavings={project.monthlySavings}
-    //           currentSavings={project.currentSavings}
-    //           targetDate={project.targetDate}
-    //         />
-    //       </div>
-    //     ))}
-    //   </div>
-    // </div>
     <>
-    <App/>
+      <div className="flex flex-col h-screen bg-gray-100">
+    
+        <main className="flex-1 overflow-y-auto p-4">
+      
+          <AccountSummary />
+
+          <div className="bg-white rounded-lg p-4 mb-4" onClick={handleClick}>
+              <div className="flex items-center text-red-600">
+                  <span className="text-sm">Mis finanzas</span>
+                  <ChevronRight className="w-4 h-4 ml-1" />
+              </div>
+          </div>
+
+          <FinanceOverview />
+
+          <TransactionList />
+
+          Sección de Proyectos
+          <h2>Mis Objetivos de Ahorro</h2>
+          <div className='project-cards'>
+            {projects.map((project) => (
+              <div key={project.id} style={{ marginBottom: '20px' }}>
+                <ProjectPlannerCard
+                projectName={project.name}
+                estimatedCost={project.estimatedCost}
+                monthlySavings={project.monthlySavings}
+                currentSavings={project.currentSavings}
+                targetDate={project.targetDate}
+                />
+            </div>
+            ))}
+          </div>
+
+        </main>
+      </div>
     </>
   )
 }
